1c021f58fa554e9e297825381d88e0d31a5885bb
samue asi <samuelasime@gmail.com>
2023-10-07 16:20:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,111 @@
+
+//group1
+const a=7
+const b =5
+ const c =12
+d=((a**b) +( b**c))/(a*c)
+
+console.log(d)
+
+//string
+const mySugPresComredeName= "AMIEBI"
+const mySenPoliceCommisionalName="whyte"
+const MyChurchSeniorBishopName="claire"
+const MyCyberLordNameIs="Lawrita"
+const mySenArmyCommisionalName="Chief David"
+console.log(mySugPresComredeName,mySenPoliceCommisionalName,
+    MyChurchSeniorBishopName,MyCyberLordNameIs,mySenArmyCommisionalName )
+
+    //number
+const oldNumbersOfTheFamily =3
+const evenNumbersOfTheFamily=4
+const PrimeNumbersOfTheFamily=5
+const compositeNumbersOfTheFamily=1
+const complexNumbersOfTheFamily=10
+const naturalNumbersOfTheFamily=9
+console.log(oldNumbersOfTheFamily,evenNumbersOfTheFamily,
+    PrimeNumbersOfTheFamily, compositeNumbersOfTheFamily,
+    complexNumbersOfTheFamily,
+)
+
+// boolean
+const AllOfUsAreMale=false
+const AllOfUsAreFemale= false
+const AllTheFemaleAreCute=false
+const AllTheMaleAreCute=true
+const AllTheBabiesAllCute=true
+
+console.log( AllOfUsAreMale,AllOfUsAreFemale,AllTheFemaleAreCute,AllTheMaleAreCute,
+AllTheBabiesAllCute
+)
+
+//undefined
+
+let  AllOfUsAreCool
+let AllOfUsAreGood
+let AllTheFemaleAreBriliant 
+let AllTheMaleAreGurus 
+let AllTheBabiesAllBeautiful 
+console.log(
+    AllOfUsAreCool,AllOfUsAreGood,AllTheFemaleAreBriliant,
+    AllTheMaleAreGurus,AllTheBabiesAllBeautiful 
+)
+
+//null
+const oldNumbersOfTheFam=null
+const evenNumsOfTheFamily= null
+const pointNumbersOfTheFamily=null
+const composNumbersOfTheFamily=null
+const compNumbersOfTheFamily=null
+const natNumbersOfTheFamily=null
+console.log(oldNumbersOfTheFam,evenNumsOfTheFamily,pointNumbersOfTheFamily,composNumbersOfTheFamily,
+    compNumbersOfTheFamily,natNumbersOfTheFamily)
+
+
+    //create an objesct of five candidates writing jambe
+const candidate1={firstNames:"success", lastNames:"George", email:"success@gmail.com",
+phoneNumbers:"0809", address:"#12.Odums",scores:"45",gender:"male",}
+candidate2={
+    firstNames:"David",lastNames:"Dango",email:"david@gmail.com",
+     phoneNumbers:"0902",address:"#4.jephtha",scores:"45",gender:"male",
+}
+candidate3={firstNames:"James",lastNames:"Peter",email:"James@gmail.com",
+ phoneNumbers:"0702",address:"#5.Alakahia", score:"200",Gender:"male"
+}
+candidate4={firstNames:"Cate",lastNames:"Samuel",email:"Cate@gmail.com", 
+phoneNumbers:"0818",address:"#15.Back Of Chem", score:"150",Gender: "female"
+}
+candidate5={firstNames:"Faith",lastNames:"Precios",email:"Fith@gmail.com", 
+phoneNumbers:"0905",address:"#9. Choba", scores:"230",gender:"female"
+
+}
+
+console.log(candidate1)
+console.log(candidate2)
+console.log(candidate3)
+console.log(candidate4)
+console.log(candidate5)
+
+
+
+//crate an object of 15itmes 
+const cars={namesOfCars:["Honder CR","Acura","Lexus Xr","Ford","Amanda","Subaru","Audi","Volvo","Mazda","TOyota Corolla","Toyata Camry","Honder","Accord","Lexus 350L","Lamborghini"],
+colorOfCars:["black","blue","red","green","white","purple","white","balck","yellow","dark green","pink","black","white","black","red"],
+prodYear:["2009","2001","2005","2017","2009","2020","2001","2002","2005","2009","2001","2004","2001","2009"]
+}
+console.log(cars.namesOfCars[0])
+console.log(cars.colorOfCars[0])
+console.log(cars.prodYear[0])
+console.log(cars.namesOfCars[1])
+console.log(cars.colorOfCars[2])
+console.log(cars.prodYear[1])
+//only viewed two items follow suit to view model of your choice
+
+
+
+//Create If or Else statement for scores of student
+const scores =100
+if(scores<=49){console.log("FAILD")}
+else if(scores<=59) {console.log("Good")}
+else if(scores<=69){console.log("Very Good")}
+else if (scores>=70) {console.log("Excellent")}
